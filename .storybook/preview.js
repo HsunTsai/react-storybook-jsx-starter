1ff9353ab744973a25230b5999f02380ef3cb648
Hsun_windows10 <t100310414@ntut.org.tw>
2021-08-29 13:31:04 +0800

    chore(eslint): eslint add max length & etc... chore(perview): add default page with docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,10 @@
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
+	viewMode: 'docs',
+	previewTabs: {
+		'storybook/docs/panel': { index: -1 },
+		// canvas: { hidden: true },
+	},
 	controls: {
 		matchers: {
 			color: /(background|color)$/i,
